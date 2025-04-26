9966901a69de06454f8b90f4d6c7a0122345b7f4
Tyler Cartier <46423276+TylerCartier106@users.noreply.github.com>
2025-04-26 13:01:51 -0400

    Landing Page Done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,29 @@ import { FaUserAlt, FaMusic, FaPlay } from 'react-icons/fa';
 export default function Home() {
   return (
     <div className={styles.container}>
+      {/* <div className={styles.artistBackground}>
+        {[
+          { src: '/images/beethovenimage.jpg', top: '10%', left: '5%' },
+          { src: '/images/kendrickimage.jpg', top: '20%', right: '10%' },
+          { src: '/images/weekendimage.png', top: '50%', left: '13%' },
+          { src: '/images/szaimage.jpg', bottom: '15%', right: '12%' },
+          { src: '/images/metallicaimage.jpg', bottom: '10%', right: '35%' },
+          { src: '/images/travisscottimage.jpg', bottom: '7%', left: '31%' },
+        ].map((artist, i) => (
+          <img
+            key={i}
+            src={artist.src}
+            alt={`artist-${i}`}
+            className={styles.floatingArtist}
+            style={{
+              top: artist.top,
+              left: artist.left,
+              right: artist.right,
+              bottom: artist.bottom,
+            }}
+          />
+        ))}
+      </div> */}
       <p className={styles.welcomeText}>
         Welcome to Riffle! Riffle is a web application that uses the publicly available Spotify API to provide users with useful statistics about their listening habits.
       </p>
